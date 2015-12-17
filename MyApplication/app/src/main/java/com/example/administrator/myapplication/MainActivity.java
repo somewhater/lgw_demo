10befee9159c4300ab97e311c65fd98e838cd311
somewhater <guowei400@gmail.com>
2015-12-17 21:02:24 +0800

    change unicode

--- MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.linearlayoutdemo);
+        setContentView(R.layout.linearlayoutdemo);
         //        setContentView(R.layout.absolutelayoutdemo);
         //        setContentView(R.layout.framelayoutdemo);
         //        setContentView(R.layout.relativelayoutdemo);
