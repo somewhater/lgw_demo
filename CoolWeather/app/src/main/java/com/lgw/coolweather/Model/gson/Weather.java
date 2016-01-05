3b63565eaf97eab0f7b3f7f751281d3a842cc3eb
liguowei <guowei400@gmail.com>
2016-01-05 16:14:13 +0800

    GSON BEAN

--- CoolWeather/app/src/main/java/com/lgw/coolweather/Model/gson/Weather.java
+++ CoolWeather/app/src/main/java/com/lgw/coolweather/Model/gson/Weather.java
@@ -1,13 +1,21 @@
 package com.lgw.coolweather.model.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.io.Serializable;
 
 /**
  * Created by Administrator on 2016-01-05.
  */
 public class Weather implements Serializable {
-    private Aqi aqi;
-    private Basic basic;
-    private Daily_forecast daily_forecast;
+    @SerializedName("HeWeather data service 3.0")
+    private Data[] datas;
 
+    public Data[] getDatas() {
+        return datas;
+    }
+
+    public void setDatas(Data[] datas) {
+        this.datas = datas;
+    }
 }
